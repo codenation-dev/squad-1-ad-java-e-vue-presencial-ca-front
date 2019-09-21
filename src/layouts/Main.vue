424b30cfd76669233cf32cdeb48e8a0510adac9a
Leonardo Leal <leonardotade@gmail.com>
2019-09-21 17:39:45 -0300

    Ajustes gerais nos demais componentes

---
+++ src/layouts/Main.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+    <Header />
+    <div class="container">
+      <router-view />
+    </div>
+  </main>
+</template>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style lang="scss">
+.container {
+  flex-grow: 1;
+  margin: 0 auto;
+  position: relative;
+  width: auto;
+}
+
+@media (min-width: $small) {
+  .container {
+  }
+}
+
+@media (min-width: $medium) {
+  .container {
+    max-width: 960px;
+  }
+}
+
+@media (min-width: $large) {
+  .container {
+    max-width: 1152px;
+  }
+}
+
+@media (min-width: $extra-large) {
+  .container {
+    max-width: 1344px;
+  }
+}
+</style>
---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <input type="text" placeholder="e-mail" aria-placeholder="e-mail" />
+    <input type="text" placeholder="password" aria-placeholder="password" />
+    <button>Login</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.login {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100px;
+  justify-content: center;
+  min-height: 100vh;
+}
+</style>
